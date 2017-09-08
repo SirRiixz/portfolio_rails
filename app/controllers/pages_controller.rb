3906dc2fa0fad8f7a84540d455c63fa8faa1c9b3
Richard <r.santos92@live.com>
2017-09-08 13:22:16 +1000

    initial commit

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,12 @@
+class PagesController < ApplicationController
+  def homepage
+    # puts "the params are: #{params[:blah]}, ok?"
+    #
+    # # hash
+    # myvar = {
+    #   :id => "luke123",
+    #   :address => "here4"
+    # }
+    # puts myvar[:address]
+  end
+end
